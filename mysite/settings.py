03ed05d7cd6c4b0127cc7f1e241985e05634eb7c
maneesha <ahseenam@gmail.com>
2014-12-07 14:51:44 -0500

    add polls to INSTALLED_APPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
+    #don't forget the trailing comma!
 )
 
 MIDDLEWARE_CLASSES = (
