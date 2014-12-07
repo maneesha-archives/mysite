91750cf6fb447ab146dd9a3aa4c7d4b6c1b029e4
maneesha <ahseenam@gmail.com>
2014-12-07 15:27:49 -0500

    update models & create data

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,9 +8,18 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __unicode__(self):
+        return u'%s %s' %(self.question, self.pub_date)
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
     
+    def __unicode__(self):
+        return u'%s %s' % (self.choice, self.votes)
+
