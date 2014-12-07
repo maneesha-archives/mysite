00c423fea4c31c801d43898797e94573fc274bc7
maneesha <ahseenam@gmail.com>
2014-12-07 15:46:58 -0500

    furthur organized poll page

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from polls.models import Poll
 
-#This sets the order of the items in the Poll
+#This sets the order of the items in the Poll and puts them in sections (aka fieldsets)
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
+    fieldsets = [
+        #first section - no heading, has the field question
+        (None, {'fields':['question']}),
+        #second section, heading = Date information, has the field pub_date
+        ('Date information',{'fields':['pub_date']}),
+        #needs a trailing comma
+    ]
 
 admin.site.register(Poll, PollAdmin)
