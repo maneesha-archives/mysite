0dea39ae99a1ae2e9be763dea76bc47ca374d334
maneesha <ahseenam@gmail.com>
2014-12-07 15:43:22 -0500

    change order of poll items in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from polls.models import Poll
 
-# Register your models here.
+#This sets the order of the items in the Poll
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
 
-admin.site.register(Poll)
+admin.site.register(Poll, PollAdmin)
