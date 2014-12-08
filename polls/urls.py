b6a191a70f62fe299697ea8f1d9a9c00152bf22b
maneesha <ahseenam@gmail.com>
2014-12-08 15:06:03 -0500

    add sample views with arguments

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,17 @@ from polls import views
 
 #mysite/urls.py includes a line that says to include this file
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    #ex: /polls/
+    url(r'^$', views.index, name='index'),
+
+    #ex: /polls/5/
+    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+
+    #ex: polls/5/results/
+    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+
+    #ex: polls/5/vote/
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 
     #url takes four arguments:
     #regex to create the url 
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#An extremely simple view that just returns the content of HttpRepsonse
 def index(request):
     return HttpResponse("Hello world. You're at the poll page.")
+
+#some views that take arguments
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
+
+    
