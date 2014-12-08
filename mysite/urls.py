ddd2668f5280e6a44cc71e3874ef153c6e9103d0
maneesha <ahseenam@gmail.com>
2014-12-08 14:44:56 -0500

    add polls/urls.py to mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    #this tells django to also include polls/urls.py
+    url(r'^polls/', include('polls.urls')), #remember the trailing comma!
 )
