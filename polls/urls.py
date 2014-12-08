ec72daf93c2c0675e57a65b6e5fe0d2f019cef71
maneesha <ahseenam@gmail.com>
2014-12-08 14:52:34 -0500

    add explanatory notes

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,11 @@ from polls import views
 #mysite/urls.py includes a line that says to include this file
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index')
+
+    #url takes four arguments:
+    #regex to create the url 
+    #the view that will create the page
+    #kwarg -- not covered here
+    #name to refer to it unambiguously
     )
 
