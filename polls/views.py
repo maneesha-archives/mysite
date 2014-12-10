a64cdc4ae95b347eb00dc44e6238856a1493b569
maneesha <ahseenam@gmail.com>
2014-12-10 15:54:44 -0500

    add results page

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
-from polls.models import Poll 
+from polls.models import Choice, Poll 
 from django.core.urlresolvers import reverse
 
 #An extremely simple view that just returns the content of HttpRepsonse
@@ -38,7 +38,11 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', {'poll':poll})
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    #placeholder page:
+    #return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll':poll})
 
 def vote(request, poll_id):
 
@@ -69,4 +73,4 @@ def vote(request, poll_id):
         #user hits the back button.
         #HttpResponseRedirect always takes only 1 argument: url of that page
         #reverse function creates the url for the results page
-        return HttpResponseRedirect(reverse('polls.results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
