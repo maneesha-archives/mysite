63c69c76e1928fa16535e9358aa7bfe3d9f3e7c4
maneesha <ahseenam@gmail.com>
2014-12-09 20:33:57 -0500

    fix field name in model & db

--- polls/models.py
+++ polls/models.py
@@ -20,7 +20,7 @@ class Poll(models.Model):
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField()
     
     def __unicode__(self):
